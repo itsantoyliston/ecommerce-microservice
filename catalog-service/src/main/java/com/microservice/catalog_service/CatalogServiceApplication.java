ddd45d379c6e1b58378ffcf68d52ab415fcec0f8
antony <antonyliston13@gmail.com>
2026-03-14 15:46:31 +0530

    Implemented catalogservice api.

--- catalog-service/src/main/java/com/microservice/catalog_service/CatalogServiceApplication.java
+++ catalog-service/src/main/java/com/microservice/catalog_service/CatalogServiceApplication.java
@@ -1,9 +1,14 @@
 package com.microservice.catalog_service;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+//@EnableConfigurationProperties
+@ConfigurationPropertiesScan
 public class CatalogServiceApplication {
 
     public static void main(String[] args) {
--- catalog-service/src/test/java/com/microservice/catalog_service/CatalogServiceApplicationTests.java
+++ catalog-service/src/test/java/com/microservice/catalog_service/CatalogServiceApplicationTests.java
@@ -4,9 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 
-@Import(ContainersConfig.class)
-@SpringBootTest
-class CatalogServiceApplicationTests {
+
+class CatalogServiceApplicationTests  extends AbstractIntegerationTest{
 
     @Test
     void contextLoads() {}
